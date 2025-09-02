a820029ca4f99b3cd81a67aae40f78a017a39c2a
Luis Barroso <luis.barroso@corilus.be>
2025-09-02 18:06:45 +0200

    Stage 3

--- app/main.go
+++ app/main.go
@@ -7,6 +7,37 @@ import (
 	"os"
 )
 
+type Request struct {
+	MessageSize int32
+	RequestApiKey int16
+	RequestApiVersion int16
+	CorrelationId int32
+	ClientId string // nullable string
+}
+
+func ParseBuffer(buffer []byte) Request {
+	var r Request
+	r.MessageSize = int32(binary.BigEndian.Uint32(buffer[0:4]))
+	r.RequestApiKey = int16(binary.BigEndian.Uint16(buffer[4:6]))
+	r.RequestApiVersion = int16(binary.BigEndian.Uint16(buffer[6:8]))
+	r.CorrelationId = int32(binary.BigEndian.Uint32(buffer[8:12]))
+
+	nullableStringLength := int16(binary.BigEndian.Uint16(buffer[12:14]))
+
+	if nullableStringLength == -1 {
+		r.ClientId = ""
+	} else {
+		r.ClientId = string(buffer[14 : 14+nullableStringLength])
+	}
+
+	return r
+}
+
+type Response struct {
+	MessageSize int32
+	CorrelationId int32
+}
+
 func main() {
 	listener, err := net.Listen("tcp", "0.0.0.0:9092")
 	if err != nil {
@@ -34,18 +65,22 @@ func main() {
 			break
 		}
 
-		response := processMessage(buffer[:numberOfBytesRead])
-		connection.Write(response)
+		request := ParseBuffer(buffer[:numberOfBytesRead])
+		response := processRequest(&request)
 
-		fmt.Println("Received message: ", string(buffer))
+		_, err = connection.Write(response)
+		if err != nil {
+			fmt.Println("Error writing to connection: ", err.Error())
+			break
+		}
 	}
 }
 
 
-func processMessage(message []byte) []byte {
+func processRequest(request *Request) []byte {
 	messageSize := createByteSliceFromInt(0)
-	correlationId := createByteSliceFromInt(7)
-	fmt.Printf("Processing message: %s\n", string(message))
+	correlationId := createByteSliceFromInt(int(request.CorrelationId))
+
 	return append(messageSize, correlationId...)
 }
 
