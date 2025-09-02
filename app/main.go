08f5c9f796df8b6edb17a10f56fecbbf6e52f789
Luis Barroso <luis.barroso@corilus.be>
2025-09-02 17:38:12 +0200

    Stage 2

--- app/main.go
+++ app/main.go
@@ -1,14 +1,11 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"net"
 	"os"
 )
-
-// Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
 
 func main() {
 	listener, err := net.Listen("tcp", "0.0.0.0:9092")
@@ -16,9 +13,44 @@ func main() {
 		fmt.Println("Failed to bind to port 9092")
 		os.Exit(1)
 	}
-	_, err = listener.Accept()
+
+	connection, err := listener.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	defer connection.Close()
+
+	buffer := make([]byte, 1024)
+
+	for {
+		numberOfBytesRead, err := connection.Read(buffer)
+		if err != nil {
+			fmt.Println("Error reading from connection: ", err.Error())
+			break
+		}
+
+		if numberOfBytesRead == 0 {
+			break
+		}
+
+		response := processMessage(buffer[:numberOfBytesRead])
+		connection.Write(response)
+
+		fmt.Println("Received message: ", string(buffer))
+	}
+}
+
+
+func processMessage(message []byte) []byte {
+	messageSize := createByteSliceFromInt(0)
+	correlationId := createByteSliceFromInt(7)
+	fmt.Printf("Processing message: %s\n", string(message))
+	return append(messageSize, correlationId...)
+}
+
+func createByteSliceFromInt(integer int) []byte {
+	arr := make([]byte, 4)
+	binary.BigEndian.PutUint32(arr, uint32(integer))
+	return arr
 }
